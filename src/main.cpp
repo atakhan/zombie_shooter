286c9de11bcbdfed709a8f4bf196891dc2065807
Myre Greenwheat <myregree@ge-l3.kzn.21-school.ru>
2024-09-26 13:57:00 +0300

    added camera;

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,7 @@ int main() {
     Game game;
     Scene scene = Scene();
 
-    // Entities
-    scene.AddEntity(new Player(
+    Player *player = new Player(
         Config::PLAYER_SPAWN_POSITION_X, 
         Config::PLAYER_SPAWN_POSITION_Y,
         Config::PLAYER_HEALTH, 
@@ -21,10 +20,12 @@ int main() {
         Config::PLAYER_AGILITY, 
         Config::PLAYER_HEARING_RADIUS, 
         Config::PLAYER_ATTACK_RADIUS
-    ));
+    );
 
-    for (size_t i = 0; i < Config::ZOMBIES_COUNT; i++)
-    {
+    // Entities
+    scene.AddEntity(player);
+
+    for (size_t i = 0; i < Config::ZOMBIES_COUNT; i++) {
         scene.AddEntity(new Zombie(
             Config::ZOMBIE_SPAWN_POSITION_X, 
             Config::ZOMBIE_SPAWN_POSITION_Y,
@@ -46,18 +47,33 @@ int main() {
     game.Init();
 
     InitWindow(
-        Config::WINDOW_WIDTH, 
-        Config::WINDOW_HEIGHT, 
+        Config::WINDOW_WIDTH,
+        Config::WINDOW_HEIGHT,
         (Config::WINDOW_TITLE).c_str()
     );
+
+    // Camera
+    Camera2D camera = { 0 };
+    camera.target = (Vector2){ Config::PLAYER_SPAWN_POSITION_X + 20.0f, Config::PLAYER_SPAWN_POSITION_Y + 20.0f };
+    camera.offset = (Vector2){ Config::WINDOW_WIDTH/2.0f, Config::WINDOW_HEIGHT/2.0f };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         game.Update();
-        
+
+        camera.target = (Vector2){ 
+            player->GetPositionX() + 20, 
+            player->GetPositionY() + 20
+        };
+
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            game.Draw();
+            BeginMode2D(camera);
+                game.Draw();
+            EndMode2D();
         EndDrawing();
     }
 
