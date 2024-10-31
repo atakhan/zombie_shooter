53b8108d3ec31f7d89dd4a1cdc6d762f1431a8b9
atakhan <kadyr.atakhanov@gmail.com>
2024-11-01 01:44:23 +0300

    refactoring draft;

---
+++ src/components/colliders/Bootstrap.h
@@ -0,0 +1,7 @@
+#ifndef SRC_COMPONENTS_COLLIDERS_BOOTSTRAP_H
+#define SRC_COMPONENTS_COLLIDERS_BOOTSTRAP_H
+
+#include "./CircleColliderComponent.h"
+#include "./RectangleColliderComponent.h"
+
+#endif // SRC_COMPONENTS_COLLIDERS_BOOTSTRAP_H
